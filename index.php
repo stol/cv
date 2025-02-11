9287a45a93a728b108e7a6cc3008deb5d40cacff
System default user <alexis.faye@hotmail.fr>
2025-02-11 10:15:23 +0100

    Upd

--- index.php
+++ index.php
@@ -26,7 +26,8 @@
                         <h1 class="header__name"><span class="header__name-label">Alexis Faye</span></h1>
                         <p class="header__infos">
                             43 ans | Paris 20e<br/>
-                            alexis.faye@hotmail.fr
+                            alexis.faye@hotmail.fr<br/>
+                            <span class="phone">6 <span>XYZ</span>74 07</span>
                         </p>
                     </div><!--
                  --><div class="layout__item palm-1/1 lap-2/4 7/10">
@@ -37,7 +38,9 @@
                         </h2>
                     </div><!--
                  --><div class="layout__item palm-1/1 lap-1/4 1/10">
-                        <img class="header__img" src="img/photo-2.jpg"/>
+                        <div class="header__img-cont">
+                            <a href="img/photo-2025b.png" target="_blank"><img class="header__img" src="img/photo-2025b.png"/></a>
+                        </div>
                     </div>
                 </div>
 
@@ -46,15 +49,13 @@
 
         <div class="content">
 
-            <?php /*
-            <h2 class="section-title"><span class="section-title__inside">Présentation</span></h2>
+            <!--h2 class="section-title"><span class="section-title__inside">Présentation</span></h2-->
 
             <section class="section">
-                <div class="section__inside">
-                    Fort de plus de 20 années la tech', j'ai appris que le principal défi est de trouver le bon compromis entre vitesse et qualité. Passé par le code puis le  management, j'ai développé une méthode pragmatique dont l'objectif est la création d'une roadmap fiable, essentielle tant pour le business que pour l'organisation des équipes. J'introduis l'agilité par l'amélioration continue (ex : rétrospectives), et je me repose sur une communication claire pour éviter les frictions, notamment du côté des priorités. Enfin, l'implication de toutes les équipes favorise à la fois l'innovation et une bonne ambiance de travail. Je reste toujours pragmatique et m'adapte aux organisations existantes.
+                <div class="section__inside section__inside--intro">
+                    En plus de 20 années passées dans la tech, j'ai travaillé sur des sujets variés (sites à fort trafic, e-commerce, intranets, réseau social…), dont certains dans un contexte internationnal. Passé du code au management, j'ai développé une méthode pragmatique dont l'objectif est la création d'une roadmap fiable, essentielle tant pour le business que pour l'organisation des équipes. Je privilégie l'implication des équipes pour favoriser à la fois l'innovation et une bonne ambiance de travail. Enfin, je reste toujours pragmatique, pensant en termes de problèmes et non en termes de personnes, et m'adapte aux organisations existantes.
                 </div>
             </section>
-            */ ?>
 
 
             <h2 class="section-title"><span class="section-title__inside">Expérience</span></h2>
@@ -68,14 +69,22 @@
                                 <div class="layout__item palm-1/1 lap-1/4 2/10">
                                     <?php /*<div class="exp__date"><?php echo $experience->title ?></div> */ ?>
                                     <div class="exp__date"><?php
-                                        echo (new DateTime($experience->start))->format("Y");
-                                        echo ' (' . dateDiff2Str($experience->start, $experience->end) . ')';
+                                        $startY = (new DateTime($experience->start))->format("Y");
+                                        $endY = (new DateTime($experience->end))->format("Y");
+                                        echo $startY;
+                                        if ($endY > $startY){
+                                            echo '-' . $endY . '<br/>';
+                                        }
+                                        echo '<span class="exp__datel">' . dateDiff2Str($experience->start, $experience->end) . '</span>';
                                     ?></div>
                                 </div><!--
                              --><div class="layout__item palm-1/1 lap-3/4 8/10 ">
                                     <div class="exp__job"><?php echo $experience->job ?></div>
                                     <?php if ($experience->link) : ?>
                                         <div class="exp__context"> @ <a href="<?php echo $experience->link ?>" class="exp__company"><?php echo $experience->company ?></a>, <?php echo $experience->area ?></div>
+                                    <?php endif ?>
+                                    <?php if ($experience->intro) : ?>
+                                    <blockquote class="exp__intro"><?php echo $experience->intro ?></blockquote>
                                     <?php endif ?>
                                     <ul class="exp__tasks">
                                     <?php foreach ($experience->tasks as $task) : ?>
